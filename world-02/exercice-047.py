afb1e6329cacd53560211283c0fb880c653be8b1
Glauber Albuquerque <glauber.sabino19@gmail.com>
2020-11-17 16:05:10 -0300

    Conclusão do exercício 047 referente ao mundo 02

--- world-02/exercice-047.py
+++ world-02/exercice-047.py
@@ -2,7 +2,8 @@
 # Crie um programa que mostre na tela todos os números pares que estão no
 # intervalo entre 1 e 50.
 
-for c in range(1, 50):
+# for c in range(2, 51, 2)
+for c in range(1, 51):
     isPar = c % 2
     if isPar == 0:
         print(c)
