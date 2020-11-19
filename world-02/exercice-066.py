1b56d9141877f0ab306ed0481545b91ff7af8ffd
Glauber Albuquerque <glauber.sabino19@gmail.com>
2020-11-19 15:54:36 -0300

    Conclusão do exercício 066 do curso de Python do mundo 02.

---
+++ world-02/exercice-066.py
@@ -0,0 +1,18 @@
+# Exercício 065
+# Crie um programa que leia números inteiros pelo teclado. O programa só vai parar quando o usuário
+# digitar o valor 999, que é a condição de parada. No final, mostre quantos números foram digitados e
+# qual foi a soma entre elas (desconsiderando o flag).
+
+n = int(input('Digite um número inteiro: '))
+s = c = 0
+while n != 999:
+    c += 1
+    s += n
+    if n == 999:
+        break
+    else:
+        n = int(input('Digite um número inteiro: '))
+print(f'''
+=> A quantidade de números digitados foi de {c}.
+=> A soma entre eles foi de {s}.
+''')
