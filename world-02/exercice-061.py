50c3951001f6355f07c88b672a2c9a14fc020936
Glauber Albuquerque <glauber.sabino19@gmail.com>
2020-11-19 08:54:42 -0300

    Conclusão do exercício 061 do curso de Python do mundo 02.

---
+++ world-02/exercice-061.py
@@ -0,0 +1,12 @@
+# Exercício 061
+# Refaça o DESAFIO 51, lendo o primeiro termo e a razão de uma PA, mostrando os 10 primeiros termos da progressão
+# usando a estrutura while.
+
+termo = int(input('Informe o primeiro termo da PA: '))
+razao = int(input('Informe a razão da PA: '))
+decimo = termo + (10 - 1) * razao
+
+c = termo
+while c <= decimo:
+    print(c)
+    c += razao
