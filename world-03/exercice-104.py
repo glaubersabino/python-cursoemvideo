4a167de0153c6abf129949dba5c97ec0b7d4dc71
Glauber Albuquerque <glauber.sabino19@gmail.com>
2020-11-26 17:53:06 -0300

    Conclusão do exercício 104 do curso de Python do mundo 03.

---
+++ world-03/exercice-104.py
@@ -0,0 +1,14 @@
+# Exercício 104
+# Crie um programa que tenha a função leiaInt(), que vai funcionar de forma semelhante ‘a função input() do Python
+# só que fazendo a validação para aceitar apenas um valor numérico. Ex: n = leiaInt(‘Digite um n: ‘)
+
+def leiaInt(text):
+    n = input(text)
+    while not n.isnumeric():
+        print('=> ERRO! Informe um número inteiro.')
+        n = input(text)
+    return n
+
+
+num = leiaInt('DIGITE UM NÚMERO INTEIRO > ')
+print(f'VOCÊ ACABOU DE DIGITAR O NÚMERO {num}.')
