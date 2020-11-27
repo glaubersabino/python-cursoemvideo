82e3e02c58aa0163331a1c0c73e70f7e8be6c765
Glauber Albuquerque <glauber.sabino19@gmail.com>
2020-11-27 11:31:42 -0300

    Conclusão do exercício 112 do curso de Python do mundo 03.

--- world-03/exercice-112.py
+++ world-03/exercice-112.py
@@ -1,9 +1,9 @@
-# Exercicio 111
+# Exercicio 112
 # Crie um pacote chamado utilidadesCeV que tenha dois módulos internos chamados moeda e dado.
 # Transfira todas as funções utilizadas nos desafios 107, 108 e 109 para o primeiro pacote e
 # mantenha tudo funcionando.
 
-from modulos.utilidadescev import moeda
+from modulos.utilidadescev import dado, moeda
 
-n = int(input('> DIGITE UM NÚMERO: '))
-moeda.resumo(n, 80, 35)
+resp = dado.leiaDinheiro('=> DIGITE UM VALOR(DINHEIRO): ')
+moeda.resumo(resp, 15, 10)
