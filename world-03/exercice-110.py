5d4cd71605b6d8604d9b57252c6e01ebd45d68d0
Glauber Albuquerque <glauber.sabino19@gmail.com>
2020-11-27 10:47:48 -0300

    Conclusão do exercício 110 do curso de Python do mundo 03.

---
+++ world-03/exercice-110.py
@@ -0,0 +1,8 @@
+# Exercicio 110
+# Adicione o módulo moeda.py criado nos desafios anteriores, uma função chamada resumo(), que mostre na
+# tela algumas informações geradas pelas funções que já temos no módulo criado até aqui.
+
+from modulos import exercice107
+
+n = int(input('> DIGITE UM NÚMERO: '))
+exercice107.resumo(n, 80, 35)
