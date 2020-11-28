ca155a929231028a5e5ab40f948aba5b7f2f8d2f
Glauber Albuquerque <glauber.sabino19@gmail.com>
2020-11-28 12:04:40 -0300

    Conclusão do exercício 115b do curso de Python do mundo 03.

--- world-03/exercice-115b.py
+++ world-03/exercice-115b.py
@@ -1,4 +1,4 @@
-# Exercicio 115
+# Exercicio 115b
 # Vamos criar um menu em Python, usando modularização.
 
 from modulos.exercice115.menu import menu
@@ -6,3 +6,6 @@ import modulos.exercice115.database as database
 
 database.create()
 opc = menu()
+
+if opc == 1:
+    database.show()
